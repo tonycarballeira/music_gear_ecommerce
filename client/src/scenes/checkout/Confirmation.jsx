830d6793fa57a9b9b1366c662af131d28fed9607
tonycarballeira <tonycarballeira@gmail.com>
2023-04-05 19:39:37 -0400

    added files for navbar, checkout, item, and home

---
+++ client/src/scenes/checkout/Confirmation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Confirmation = () => {
+  return (
+    <div>
+      COnsfirmation
+    </div>
+  )
+}
+
+export default Confirmation
+COnsfirmation
--- client/src/theme.js
+++ client/src/theme.js
@@ -61,7 +61,19 @@ export const theme = createTheme({
         h1: {
             fontFamily: [ "Cinzel", "sans-serif"].join(","),
             fontSize: 48,
-        }
+        },
+        h2: {
+            fontFamily: [ "Cinzel", "sans-serif"].join(","),
+            fontSize: 36,
+        },
+        h3: {
+            fontFamily: [ "Cinzel", "sans-serif"].join(","),
+            fontSize: 20,
+        },
+        h4: {
+            fontFamily: [ "Cinzel", "sans-serif"].join(","),
+            fontSize: 48,
+        },
     }
 })
 
