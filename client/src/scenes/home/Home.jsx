4beb4285e7e5a56f0650c0852782f99a81bc9258
tonycarballeira <tonycarballeira@gmail.com>
2023-04-06 16:43:48 -0400

    imported components to home page

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import ShoppingList from "./ShoppingList";
+import Subscribe from "./Subscribe";
+import MainCarousel from "./MainCarousel";
 
-const Home = () => {
+function Home() {
   return (
-    <div>
-      Home
+    <div className="home">
+      <MainCarousel />
+      <ShoppingList />
+      <Subscribe />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
