800d4bdd42f2676e34c74f6b6df0a2bc9e4492c8
tonycarballeira <tonycarballeira@gmail.com>
2023-04-06 15:03:01 -0400

    imported redux global function into navbar. using material ui badge. calling redux function at cart button

--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -9,10 +9,12 @@ import {
 } from "#mui/icons-material";
 import { useNavigate} from "react-router-dom";
 import { shades } from "../../theme";
+import { setIsCartOpen } from '../../state';
 
 const Navbar = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const useSelector((state) => state.cart.cart);
 
   return (
     <Box
@@ -55,9 +57,27 @@ const Navbar = () => {
               <PersonOutlined />
             </IconButton>
 
-            <IconButton sx={{color: "black"}}>
-              <ShoppingBagOutlined />
-            </IconButton>
+            <Badge 
+              badgeContent={cart.length}
+              color="secondary"
+              invisible={cart.length === 0}
+              sx={{
+                "& .MuiBadge-badge": {
+                  right: 5,
+                  top: 5,
+                  padding: "0 4px",
+                  height: "14px",
+                  minWidth: "13px"
+                }
+              }}
+            >
+              <IconButton 
+                onCLick={() => dispatch(setIsCartOpen({}))}
+                sx={{color: "black"}}>
+                  
+                <ShoppingBagOutlined />
+              </IconButton>
+            </Badge>
 
             <IconButton sx={{color: "black"}}>
               <MenuOutlined />
